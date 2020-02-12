e6e3f2e6c506cf3eccdc15b4c7c79c47313a48e2
Girish Nandgawe <girish.nandgawe@gmail.com>
2020-02-12 18:28:16 +0530

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -28,12 +28,8 @@ namespace DotNetCoreSqlDb
             services.AddMvc();
 
             // Use SQL Database if in Azure, otherwise, use SQLite
-            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
-                services.AddDbContext<MyDatabaseContext>(options =>
+           services.AddDbContext<MyDatabaseContext>(options =>
                         options.UseSqlServer(Configuration.GetConnectionString("MyDbConnection")));
-            else
-                services.AddDbContext<MyDatabaseContext>(options =>
-                        options.UseSqlite("Data Source=localdatabase.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
